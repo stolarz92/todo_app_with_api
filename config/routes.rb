2e88b676ece73e302cd55f43b25b94b6021fc371
stolarz <stolarz92@gmail.com>
2018-06-02 21:30:45 +0200

    Add homepage controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :homepage, only: [:index]
   resources :todo_lists do
     resources :list_items do
       member do
@@ -8,7 +9,7 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'todo_lists#index'
+  root 'homepage#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #JSON API
