bd6af2d1dea2cf895d2b64ebe36e673afe5c6bc2
stolarz <stolarz92@gmail.com>
2018-06-02 22:01:24 +0200

    Revert "Add homepage controller"
    This reverts commit 2e88b676ece73e302cd55f43b25b94b6021fc371.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :homepage, only: [:index]
   resources :todo_lists do
     resources :list_items do
       member do
@@ -9,7 +8,7 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'homepage#index'
+  root 'todo_lists#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #JSON API
