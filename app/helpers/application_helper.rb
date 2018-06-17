29e05a966f4f312115ebd9effedc31f379788ff4
stolarz <stolarz92@gmail.com>
2018-06-17 13:44:07 +0200

    UI improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def active_class(link_path)
-    if current_page?(link_path) || current_page?(root_path)
+  def active_class(link_path, root = false)
+    if current_page?(link_path) || current_page?(root_path) && root
       "menu-active"
     else
       ""
