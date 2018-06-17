d73a7598c3f7a94c0d0a6360561baf5cc52bfc99
stolarz <stolarz92@gmail.com>
2018-06-17 14:34:42 +0200

    UI improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 module ApplicationHelper
   def active_class(link_path, root = false)
-    if current_page?(link_path) || current_page?(root_path) && root
+    if current_page?(link_path) ||
+       current_page?(root_path) && root ||
+       link_path.include?('todo_lists') && request.controller_class == TodoListsController
       "menu-active"
     else
       ""
